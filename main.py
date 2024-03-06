7013c3795a8ca827fed9b47850bd1849fa7f77ab
AIOnGraph <147408447+AIOnGraph@users.noreply.github.com>
2024-03-06 13:59:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ with st.sidebar:
     st.write('2. Upload a pdf')
     st.write('3. Ask a question about the document💬')
 
-    OpenAPIAI = st.text_input('OpenAI API Key 🔑',placeholder='Paste your key(🔑) here',type='passward')
+    OpenAPIAI = st.text_input('OpenAI API Key 🔑',placeholder='Paste your key(🔑) here',type='password')
     if not OpenAPIAI:
         st.warning(body='Kindly enter you API 🔑 in the side bar to chat with us',icon='⚠️')
 
